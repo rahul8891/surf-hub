292be30fac71bea63e8ddcf438e02457e1181eec
rafi ahmad <rafi.ahmad@evontech.com>
2020-10-30 18:31:57 +0530

    fix bug

---
+++ database/migrations/2020_10_30_060455_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->char('country_code', 4)->nullable();
+            $table->smallInteger('flag')->default('1')->comment('1=>active,0=>deleted'); 
+            $table->timestamps();
+            $table->index(['name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
---
+++ database/migrations/2020_10_30_060923_create_states_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('state_code')->nullable();
+            $table->mediumInteger('country_id')->nullable();
+            $table->char('country_code', 4)->nullable();
+            $table->smallInteger('flag')->default('1')->comment('1=>active,0=>deleted'); 
+            $table->timestamps();
+            $table->index(['name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
