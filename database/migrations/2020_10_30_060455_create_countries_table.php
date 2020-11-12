22bb4c2048fb57e6adec3034da2662461c24265e
Ubuntu <ubuntu@ip-172-31-39-82.us-east-2.compute.internal>
2020-11-12 10:21:36 +0000

    update

--- database/migrations/2020_10_30_060455_create_countries_table.php
+++ database/migrations/2020_10_30_060455_create_countries_table.php
@@ -14,12 +14,16 @@ class CreateCountriesTable extends Migration
     public function up()
     {
         Schema::create('countries', function (Blueprint $table) {
-            $table->id();
+           $table->id();
             $table->string('name')->nullable();
             $table->char('country_code', 4)->nullable();
+            $table->char('country_code2', 4)->nullable();
+            $table->string('phone_code')->nullable();
+            $table->string('capital')->nullable();
+            $table->string('currency')->nullable();
             $table->smallInteger('flag')->default('1')->comment('1=>active,0=>deleted'); 
             $table->timestamps();
-            $table->index(['name']);
+            $table->index(['name','country_code','phone_code']);
         });
     }
 
--- resources/views/layouts/user/user_footer.blade.php
+++ resources/views/layouts/user/user_footer.blade.php
@@ -19,7 +19,7 @@
                     </a>
                     <div class="profileChangePswd">
                         <ul>
-                            <li><a href="{{ route('profile.show') }}">Profile</a></li>
+                            <li><a href="javascript:void(0)">Profile</a></li>
                             <li><a href="{{ route('showPassword') }}">Change Password</a></li>
                         </ul>
                     </div>
