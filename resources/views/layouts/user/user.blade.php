55cfee82bab6d14ab87b7c65354d596e9fee1b88
unknown <qazi.raza@evontech.com>
2021-03-02 12:11:20 +0530

    Beach break location functionality

--- resources/views/layouts/user/user.blade.php
+++ resources/views/layouts/user/user.blade.php
@@ -63,7 +63,8 @@
     <script src="{{ asset("/js/croppie.js")}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src=" https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.5.1/jquery.nicescroll.min.js"> </script>
-    
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5JiYXogWVNPfX_L4uA0oWb-qiNSfKfYk"
+  type="text/javascript"></script>
     {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/js/star-rating.min.js"></script>
     <script src="{{ asset("/js/bootstrap.js")}}"></script>
@@ -147,7 +148,7 @@
         } else if (elem.msRequestFullscreen) { /* IE11 */
           elem.msRequestFullscreen();
         }
-    }
+    }    
     </script>
 
     
--- resources/views/user/myhub.blade.php
+++ resources/views/user/myhub.blade.php
@@ -83,7 +83,7 @@
                                                     <span class="divider"></span>
                                                 </li>
                                                 <li>
-                                                    <a href="#">
+                                                    <a href="#" data-toggle="modal" data-target="#beachLocationModal" data-lat="{{$myHub->beach_breaks->latitude}}" data-long="{{$myHub->beach_breaks->longitude}}" data-id="{{$myHub->id}}" class="locationMap">
                                                         <img src="{{ asset("/img/maps-and-flags.png")}}" alt="">
                                                     </a>
                                                 </li>
@@ -362,5 +362,6 @@
             </div>
         </div>
 </section>
+@include('elements/location_popup_model')
 @include('layouts/models/upload_video_photo')
 @endsection
