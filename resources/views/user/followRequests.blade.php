c9684598fce81afbc25a5236199e06f5b354445b
Rahul Rawat <rahul.rawat@evontech.com>
2021-07-20 14:47:38 +0530

    resolve name issue

--- resources/views/user/followRequests.blade.php
+++ resources/views/user/followRequests.blade.php
@@ -8,20 +8,19 @@
                 @if (count($followRequests) > 0)
                 <div class="requests" id="allFollower">
                     @foreach ($followRequests as $key => $requests)
-                    
                     <div class="" id="row-id{{$requests->id}}">
                         <div class="userInfo">
-                            @if($requests->followed->profile_photo_path)
+                            @if($requests->follower->profile_photo_path)
                             <div class="imgWrap">
-                                <img src="{{ asset('storage/'.$requests->followed->profile_photo_path) }}" class="profileImg" alt="">
+                                <img src="{{ asset('storage/'.$requests->follower->profile_photo_path) }}" class="profileImg" alt="">
                             </div>
                             @else
                             <div class="imgWrap no-img">
-                                {{ucwords(substr($requests->followed->user_profiles->first_name,0,1))}}{{ucwords(substr($requests->followed->user_profiles->last_name,0,1))}}
+                                {{ucwords(substr($requests->follower->user_profiles->first_name,0,1))}}{{ucwords(substr($requests->follower->user_profiles->last_name,0,1))}}
                             </div>
                             @endif
                             <div class="pl-3">
-                                <h4>{{ucfirst($requests->followed->user_profiles->first_name)}} {{ucfirst($requests->followed->user_profiles->last_name)}}</h4>
+                                <h4>{{ucfirst($requests->follower->user_profiles->first_name)}} {{ucfirst($requests->follower->user_profiles->last_name)}}</h4>
                             </div>
                         </div>
                         <button class="acceptBtn accept mr-3 ml-auto" data-id="{{$requests->id}}">ACCEPT</button>
