ded29a99ab50594d0013593c171fa92ecefb1495
rahulrawat <rahul.rawat@evontech.com>
2021-10-05 23:45:45 +0530

    added reort code

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -195,30 +195,33 @@
                                         </li>
                                         <li>
                                             <a href="javascript:void(0)">REPORT
-                                                <div class="saveInfo infoHover reasonHover">
-                                                    <div class="pos-rel">
-                                                        <img src={{asset("img/tooltipArrowDown.png")}} alt="">
-                                                        <div class="text-center reportContentTxt">Report Content</div>
-                                                        <div class="reason">
-                                                            <input type="checkbox" id="Report1" name="Report1"
-                                                                value="Report">
-                                                            <label for="Report1">Report Info as incorrect</label>
+                                                <form role="form" method="POST" name="report{{$posts->id}}" action="{{ route('report') }}">
+                                                    @csrf
+                                                    <input type="hidden" class="postID" name="post_id" value="{{$posts->id}}">
+                                                    <div class="saveInfo infoHover reasonHover">
+                                                        <div class="pos-rel">
+                                                            <img src={{asset("img/tooltipArrowDown.png")}} alt="">
+                                                            <div class="text-center reportContentTxt">Report Content</div>
+                                                            <div class="reason">
+                                                                <input type="checkbox" id="Report1" name="incorrect" value="1">
+                                                                <label for="Report1">Report Info as incorrect</label>
+                                                            </div>
+                                                            <div class="cstm-check pos-rel">
+                                                                <input type="checkbox" id="Report2" name="inappropriate" value="1">
+                                                                <label for="Report2">Report content as inappropriate</label>
+                                                            </div>
+                                                            <div class="cstm-check pos-rel">
+                                                                <input type="checkbox" id="Report3" name="tolls" value="1">
+                                                                <label for="Report3">Report tolls</label>
+                                                            </div>
+                                                            <div>
+                                                                Additional Comments:
+                                                                <textarea name="comments" class="reportOnPost" id="{{$posts->id}}"></textarea>
+                                                            </div>
+                                                            <button type="submit" class="btn btn-info postReport" id="submitReport{{$posts->id}}">REPORT</button>
                                                         </div>
-                                                        <div class="cstm-check pos-rel">
-                                                            <input type="checkbox" id="Report3">
-                                                            <label for="Report3">Report content as inappropriate</label>
-                                                        </div>
-                                                        <div class="cstm-check pos-rel">
-                                                            <input type="checkbox" id="Report4">
-                                                            <label for="Report4">Report tolls</label>
-                                                        </div>
-                                                        <div>
-                                                            Additional Comments:
-                                                            <textarea></textarea>
-                                                        </div>
-                                                        <button>REPORT</button>
                                                     </div>
-                                                </div>
+                                                </form>
                                             </a>
                                         </li>
                                         @endif
