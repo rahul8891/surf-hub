74760b57468f148145def1766952affc5aa7f5c4
rahulrawat <rahul.rawat@evontech.com>
2021-10-12 17:47:54 +0530

    search beack/break list

--- resources/views/layouts/user/edit_video_layout.blade.php
+++ resources/views/layouts/user/edit_video_layout.blade.php
@@ -219,10 +219,10 @@
                                     <div class="col-md-5 col-sm-7">
                                         <div class="pos-rel">
                                             <input type="text" value="{{ old('local_beach_break',$beach_name)}}"
-                                                placeholder="Search Beach Break" class="form-control search-box2">
+                                                placeholder="Search Beach Break" class="form-control search-box2" autocomplete="off" />
                                             <input type="hidden" value="{{ Request::get('local_beach_break_id') ? "selected" : "" }}" name="local_beach_break_id"
                                                 id="local_beach_break_id2" class="form-control">
-                                            <!-- <div class="auto-search searchTwo" id="country_list2"></div> -->
+                                            <div class="auto-search searchTwo" id="country_list2"></div>
                                             
                                         </div>
                                     </div>
