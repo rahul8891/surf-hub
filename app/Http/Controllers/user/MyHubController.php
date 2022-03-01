a9512460a21db88710f9ac9a67e8ca18b04b99a7
rahulrawat <rahul.rawat@evontech.com>
2022-03-01 13:12:59 +0530

    change verify mail header

--- app/Http/Controllers/user/MyHubController.php
+++ app/Http/Controllers/user/MyHubController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 use App\Services\MasterService;
 use App\Services\UserService;
 use App\Services\PostService;
