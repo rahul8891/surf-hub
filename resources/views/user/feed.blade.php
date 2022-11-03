ef2f831aade8c7edbbcec044909af5094b7f4bc1
sagar-evon <sagar.sharma@evontech.com>
2022-11-03 19:27:11 +0530

    dev

--- resources/views/user/feed.blade.php
+++ resources/views/user/feed.blade.php
@@ -44,16 +44,22 @@
                             @endif
                         </div>
                         @if(!empty($posts->upload->image))
-                        <img src="{{ env('FILE_CLOUD_PATH').'images/'.$posts->user->id.'/'.$posts->upload->image }}" alt="Feed" class="w-100" id="myImage{{$posts->id}}">
+                        <div class="newsFeedImgVideo">
+                            <img src="{{ env('FILE_CLOUD_PATH').'images/'.$posts->user->id.'/'.$posts->upload->image }}" alt="" id="myImage{{$posts->id}}" class="postImg">
+                        </div>
                         @elseif(!empty($posts->upload->video))
                         @if (!File::exists($posts->upload->video))
+                        <div class="newsFeedImgVideo">
                         <video width="100%" preload="auto" data-setup="{}" controls autoplay playsinline muted class="video-js" id="myImage{{$posts->id}}">
                             <source src="{{ env('FILE_CLOUD_PATH').'videos/'.$posts->user->id.'/'.$posts->upload->video }}" >    
                         </video>
+                        </div>    
                         @else
+                        <div class="newsFeedImgVideo">
                         <video width="100%" preload="auto" data-setup="{}" controls autoplay playsinline muted class="video-js" id="myImage{{$posts->id}}">
                             <source src="{{ env('FILE_CLOUD_PATH').'videos/'.$posts->user->id.'/'.$posts->upload->video }}" >    
                         </video>
+                        </div>
                         @endif
                         @endif
                         <div class="user-bottom-options">
@@ -110,6 +116,10 @@
                                         </div>
                                     </div>
                                 </div>
+                                @if(Auth::user() && $posts->user_id == Auth::user()->id)
+                                <a href="{{route('deleteUserPost', Crypt::encrypt($posts->id))}}"  onclick="return confirm('Do you really want to delete this footage?')"><img src="/img/delete.png" alt="Delete"></a>
+                                @endif
+                                <!--<a href="javascript:void(0)" class="editBtn editBtnVideo" data-id="{{ $posts->id }}">EDIT</a>-->
                                 <div class="d-inline-block tag dropdown" title="Tag">
                                     <button class="btn p-0 dropdown-toggle" data-bs-toggle="dropdown"
                                             aria-expanded="false">
@@ -223,6 +233,7 @@
     </div>
 </section>
 @include('elements/location_popup_model')
+@include('layouts/models/edit_image_upload')
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script type="text/javascript">
 	var page = 1;
