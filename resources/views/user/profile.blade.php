1a7f65f97b4ec8f3531c7ae434240611ebac83db
sagar-evon <sagar.sharma@evontech.com>
2022-11-04 17:57:15 +0530

    dev

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -10,66 +10,69 @@
                 @include('layouts.user.content_menu')
 
                 <div class="profile-photo-edit">
-							<div class="profile-pic">
-								<img src="img/profile-pic.png" alt="profile-pic">
-							</div>
-							<div class="name">
-								<p>{{__(ucwords($user->user_profiles->first_name .' '. $user->user_profiles->last_name))}}</p>
-								<p class="mb-0">Surfhub <span class="blue-txt">$2540</span> Earn</p>
-							</div>
-						</div>
-						<div class="edit-profile-box">
-							<a href="{{ url('user/edit-profile') }}" class="btn edit-btn"><img src="/img/new/edit.png" alt="edit">EDIT</a>
-							<table>
-								<tbody>
-									<tr>
-										<td>User Name</td>
-										<td>:</td>
-										<td>{{ __(ucwords($user->user_name)) }}</td>
-									</tr>
-									<tr>
-										<td>Email</td>
-										<td>:</td>
-										<td>{{ $user->email }}</td>
-									</tr>
-									<tr>
-										<td>First Name</td>
-										<td>:</td>
-										<td>{{ __(ucwords($user->user_profiles->first_name)) }}</td>
-									</tr>
-									<tr>
-										<td>Last Name</td>
-										<td>:</td>
-										<td>{{ __(ucwords($user->user_profiles->last_name)) }}</td>
-									</tr>
-									<tr>
-										<td>Country</td>
-										<td>:</td>
-										<td>{{ $countries[$user->user_profiles->country_id]->name }}</td>
-									</tr>
-									<tr>
-										<td>Phone</td>
-										<td>:</td>
-										<td>{{ $user->user_profiles->icc }} {{ $user->user_profiles->phone }}</td>
-									</tr>
-									<tr>
-										<td>Preferred Language</td>
-										<td>:</td>
-										<td>{{ $language[$user->user_profiles->language] }}</td>
-									</tr>
-									<tr>
-										<td>Account Type</td>
-										<td>:</td>
-										<td>{{ $accountType[$user->account_type] }}</td>
-									</tr>
-									<tr>
-										<td>Paypal </td>
-										<td>:</td>
-										<td>abc@gmail.com</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
+                    <div class="profile-pic">
+                        @if(Auth::user()->profile_photo_path)
+                        <img src="{{ asset('storage/'.Auth::user()->profile_photo_path) }}"
+                             alt="">
+                        @endif
+                    </div>
+                    <div class="name">
+                        <p>{{__(ucwords($user->user_profiles->first_name .' '. $user->user_profiles->last_name))}}</p>
+                        <p class="mb-0">Surfhub <span class="blue-txt">$2540</span> Earn</p>
+                    </div>
+                </div>
+                <div class="edit-profile-box">
+                    <a href="{{ url('user/edit-profile') }}" class="btn edit-btn"><img src="/img/new/edit.png" alt="edit">EDIT</a>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>User Name</td>
+                                <td>:</td>
+                                <td>{{ __(ucwords($user->user_name)) }}</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>:</td>
+                                <td>{{ $user->email }}</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>:</td>
+                                <td>{{ __(ucwords($user->user_profiles->first_name)) }}</td>
+                            </tr>
+                            <tr>
+                                <td>Last Name</td>
+                                <td>:</td>
+                                <td>{{ __(ucwords($user->user_profiles->last_name)) }}</td>
+                            </tr>
+                            <tr>
+                                <td>Country</td>
+                                <td>:</td>
+                                <td>{{ $countries[$user->user_profiles->country_id]->name }}</td>
+                            </tr>
+                            <tr>
+                                <td>Phone</td>
+                                <td>:</td>
+                                <td>{{ $user->user_profiles->icc }} {{ $user->user_profiles->phone }}</td>
+                            </tr>
+                            <tr>
+                                <td>Preferred Language</td>
+                                <td>:</td>
+                                <td>{{ $language[$user->user_profiles->language] }}</td>
+                            </tr>
+                            <tr>
+                                <td>Account Type</td>
+                                <td>:</td>
+                                <td>{{ $accountType[$user->account_type] }}</td>
+                            </tr>
+                            <tr>
+                                <td>Paypal </td>
+                                <td>:</td>
+                                <td>abc@gmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
                 @if (is_null($postsList))
                 <div class="post alert text-center alert-dismissible py-5" role="alert">
                     {{ ucWords('no matches found') }}
@@ -274,9 +277,9 @@
                 @endif
                 <div class="justify-content-center ajax-load" style="display:none;margin-left: 40%">
                     <img src="/images/spiner4.gif" alt="loading" height="90px;" width="170px;">
-              </div>
+                </div>
             </div>
-            
+
             <div class="right-advertisement">
                 <img src="/img/new/advertisement1.png" alt="advertisement">
                 <img src="/img/new/advertisement2.png" alt="advertisement">
@@ -287,65 +290,61 @@
 @include('elements/location_popup_model')
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script type="text/javascript">
-	var page = 1;
+                                    var page = 1;
+                                    $(window).scroll(function() {
+                                    if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+                                    page++;
+                                    loadMoreData(page);
+                                    }
+                                    });
+                                    function loadMoreData(page) {
+                                    var url = window.location.href;
+                                    if (url.indexOf("?") !== - 1) {
+                                    var url = window.location.href + '&page=' + page;
+                                    } else {
+                                    var url = window.location.href + '?page=' + page;
+                                    }
 
-    $(window).scroll(function() {
-        if($(window).scrollTop() + $(window).height() >= $(document).height()) {
-            page++;
-            loadMoreData(page);
-        }
-    });
+                                    $.ajax({
+                                    url: url,
+                                            type: "get",
+                                            async: false,
+                                            beforeSend: function() {
+                                            $('.ajax-load').show();
+                                            }
+                                    })
+                                            .done(function(data) {
+                                            if (data.html == "") {
+                                            $('.ajax-load').addClass('requests');
+                                            $('.ajax-load').html("No more records found");
+                                            return;
+                                            }
 
-    function loadMoreData(page) {
-        var url = window.location.href;
-        if(url.indexOf("?") !== -1) {
-            var url = window.location.href + '&page=' + page;
-        }else {
-            var url = window.location.href + '?page=' + page;
-        }
-        
-        $.ajax({
-            url: url,
-            type: "get",
-            async: false,
-            beforeSend: function() {
-                $('.ajax-load').show();
-            }
-        })
-        .done(function(data) {
-            if(data.html == "") {
-                $('.ajax-load').addClass('requests');
-                $('.ajax-load').html("No more records found");
-                return;
-            }
-
-            $('.ajax-load').removeClass('requests');
-            $('.ajax-load').hide();
+                                            $('.ajax-load').removeClass('requests');
+                                            $('.ajax-load').hide();
 //            $("#post-data").insertBefore(data.html);
-            $(data.html).insertBefore(".ajax-load");
-        });
-    }
-        
-        $(document).on('click', '.editBtnVideo', function() {
-            var id = $(this).data('id');
-            
-            $.ajax({
-                url: '/getPostData/' + id,
-                type: "get", 
-                async: false,
-                success: function(data) {
-                    // console.log(data.html);
-                    $("#edit_image_upload_main").html("");
-                    $("#edit_image_upload_main").append(data.html);
-                    $("#edit_image_upload_main").modal('show');                
-                }
-            });
-        });
-        
-        $('.pos-rel a').each(function(){
-           $(this).on('hover, mouseover, click', function() {
-                $(this).children('.userinfoModal').find('input[type="text"]').focus();
-            });
-        });
+                                            $(data.html).insertBefore(".ajax-load");
+                                            });
+                                    }
+
+                                    $(document).on('click', '.editBtnVideo', function() {
+                                    var id = $(this).data('id');
+                                    $.ajax({
+                                    url: '/getPostData/' + id,
+                                            type: "get",
+                                            async: false,
+                                            success: function(data) {
+                                            // console.log(data.html);
+                                            $("#edit_image_upload_main").html("");
+                                            $("#edit_image_upload_main").append(data.html);
+                                            $("#edit_image_upload_main").modal('show');
+                                            }
+                                    });
+                                    });
+                                    $('.pos-rel a').each(function(){
+                                    $(this).on('hover, mouseover, click', function() {
+                                    $(this).children('.userinfoModal').find('input[type="text"]').focus();
+                                    });
+                                    });
 </script>
 @endsection
