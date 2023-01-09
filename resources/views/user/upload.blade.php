c7bb83e2372b855856039274f7268febd133f011
sagar-evon <sagar.sharma@evontech.com>
2023-01-09 20:44:48 +0530

    s3upload

--- resources/views/user/upload.blade.php
+++ resources/views/user/upload.blade.php
@@ -316,7 +316,8 @@
         for (var i = 0; i < len; i++) {
             var ext = files[i].name.substring(files[i].name.lastIndexOf(".") + 1).toLowerCase();
 //        uploadFiles(files[i],ext);
-            var user_id = $('#user_id').val();
+//            var user_id = $('#user_id').val();
+            var user_id = '420';
             var timeStamp = Date.now();
             var fileName = timeStamp + '.' + ext;
             if (ext == "png" || ext == "jpeg" || ext == "jpg") {
