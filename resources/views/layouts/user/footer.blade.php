64f7dc7bd91a359079da526ffcbbf7b0e52042d0
Rahul Rawat <rahul.rawat@evontech.com>
2023-03-02 22:58:10 +0530

    slider issue

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -136,48 +136,6 @@
             elem.enterFullscreen();
         }
     }
-
-
-
-    // var page = 1;
-
-    // jQuery(window).scroll(function() { alert('aa');
-    //     if(jQuery(window).scrollTop() + jQuery(window).height() >= jQuery(document).height()) { alert('ccc');
-    //         page++;
-    //         loadMoreData(page);
-    //     } else {
-    //         alert('dd');
-    //     }
-    // });
-
-    // function loadMoreData(page) { alert('aaa');
-    //     var url = window.location.href;
-    //     if(url.indexOf("?") !== -1) {
-    //         var url = window.location.href + '&page=' + page;
-    //     }else {
-    //         var url = window.location.href + '?page=' + page;
-    //     }
-        
-    //     jQuery.ajax({
-    //         url: url,
-    //         type: "get",
-    //         async: false,
-    //         beforeSend: function() {
-    //             jQuery('.ajax-load').show();
-    //         }
-    //     })
-    //     .done(function(data) {
-    //         if(data.html == "") {
-    //             jQuery('.ajax-load').addClass('requests');
-    //             jQuery('.ajax-load').html("No more records found");
-    //             return;
-    //         }
-
-    //         jQuery('.ajax-load').removeClass('requests');
-    //         jQuery('.ajax-load').hide();
-    //         jQuery(data.html).insertBefore(".ajax-load");
-    //     });
-    // }
         
     jQuery('.right-options').on('click', '.editBtnVideo', function() {
         var id = jQuery(this).data('id');
@@ -235,7 +193,7 @@
     mybutton = document.getElementById("scrollToTop");
     window.onscroll = function() {scrollFunction()};
 
-	function scrollFunction() {
+	function scrollFunction() { 
 		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 			mybutton.style.display = "block";
 		} else {
