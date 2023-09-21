daf3422a1360e42b825455789376fd14d9311356
Nitish Rawat <nitish.rawat@evontech.com>
2023-09-21 12:47:03 +0530

    Add filter and sorting for admin user in feed page.

--- resources/views/admin/dashboard/admin_feed.blade.php
+++ resources/views/admin/dashboard/admin_feed.blade.php
@@ -9,7 +9,7 @@
                 @include('layouts.admin.admin_left_sidebar')
             </div>
             <div class="middle-content" id="post-data">
-
+                @include('layouts.user.content_menu')
 
                 @if (isset($postsList) && empty($postsList[0]))
                 <div class="post alert text-center alert-dismissible py-5" role="alert">
