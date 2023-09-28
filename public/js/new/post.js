11b49cca4ab127e5c66de4292791763213ea9671
Nitish Rawat <nitish.rawat@evontech.com>
2023-09-28 17:21:16 +0530

    Resolve issue

--- public/js/new/post.js
+++ public/js/new/post.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function () {
     var csrf_token = jQuery('meta[name="csrf-token"]').attr('content');
-
+    setTimeout(function(){
+       jQuery("div.alert").fadeOut();
+    }, 2000 ); // 2 secs
     /**
      * resetForm Form  validator on close     
      * remove image preview on close (pip)
@@ -614,7 +616,7 @@ jQuery(document).on('click', function(){
     }, 2000 ); // 2 secs
 });
 
-jQuery(document).on('load', function(){
+jQuery(document).load(function(){
     setTimeout(function(){
        jQuery("div.alert").fadeOut();
     }, 2000 ); // 2 secs
